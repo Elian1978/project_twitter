46f84f9f561baa792f7452b648abeffcf3f19dfb
Elian Rojas <impresosgosen@gmail.com>
2021-03-19 20:13:17 -0600

    CREATED APPI REST

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: 'json'} do
+    resources :tweets, :path => "news"
+  end
+
+  get '/api/:fecha1/:fecha2', to: 'api/tweets#between_dates'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :tweets do 
